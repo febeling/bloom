3fd45f093a0324cb0a87e3ceff9dc0fc278368be
Caspar Florian Ebeling <florian.ebeling@nugg.ad>
2010-05-20 10:40:04 +0200

    implementation of "add" and "contains?", with tests.

--- src/bloom/core.clj
+++ src/bloom/core.clj
@@ -1,1 +1,70 @@
-(ns bloom.core)
+(ns bloom.core
+  (:import java.security.MessageDigest)
+  (:import java.nio.charset.Charset)
+  (:refer-clojure :exclude [hash contains?]))
+
+(def message-digest (MessageDigest/getInstance "SHA1"))
+
+(def charset (Charset/forName "UTF-8"))
+
+(defn optimal-k
+  "Optimal number K of hash functions for Bloom filter of bit size M
+and number of elements N
+
+  (m/n)ln2"
+  [m n]
+  (-> m
+      double
+      (/ n)
+      (* (Math/log 2.0))
+      (Math/round)))
+
+;; (defn optimal-m
+;;   "Calculate the optimal bit size M for a number of elements N and a
+;; false-positive probability of P"
+;;   [n p]
+;;   (Math/round (- 
+;; 	       (/ (* n (Math/log p))
+;; 		  (Math/pow (Math/log 2.0) 2.0)))))
+
+(defn create-bloom
+  "Create a Bloom filter with bit size M, number of (expected)
+elements N and number of hash functions K. K can be calculated."
+  ([m n k1]
+     (let [bf (atom {})]
+       (swap! bf assoc :m m)
+       (swap! bf assoc :n n)
+       (swap! bf assoc :k k1)
+       (swap! bf assoc :f 0)
+       bf))
+  ([m n] (create-bloom m n (optimal-k m n))))
+
+(defn bytes->num [bs]
+  (->> bs
+       (map #(bit-and 0xff %))
+       (reduce #(bit-or (bit-shift-left %1 8) %2) 0)))
+
+(defn hash [#^String x]
+  (let [bs (.getBytes x charset)]
+    (bytes->num (. message-digest digest bs))))
+
+(defn indexes [s m k]
+  (->> (range k)
+       (map #(str s %))
+       (map hash)
+       (map #(mod % m))))
+
+(defn add [bloom x]
+  (let [s (pr-str x)
+	{:keys [m k f]} @bloom]
+    (loop [idx (indexes s m k)
+	   f0 f]
+      (let [f1 (bit-set f0 (first idx))]
+	(if-not (next idx)
+	  (swap! bloom assoc :f f1)
+	  (recur (rest idx) f1))))))
+
+(defn contains? [bloom x]
+  (let [s (pr-str x)
+	{:keys [m k f]} @bloom]
+    (every? #(bit-test f %) (indexes s m k))))
--- test/bloom/core_test.clj
+++ test/bloom/core_test.clj
@@ -1,3 +1,68 @@
 (ns bloom.core-test
   (:use [bloom.core] :reload-all)
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  (:refer-clojure :exclude [hash contains?]))
+
+(deftest k-test
+  ;; Expectations take from:
+  ;; http://code.google.com/p/java-bloomfilter/source/browse/trunk/test/com/skjegstad/utils/BloomFilterTest.java
+  (are [k m n] (= k (optimal-k m n))
+       1 2 1
+       2 3 1
+       3 4 1
+       3 5 1
+       4 6 1
+       5 7 1
+       6 8 1
+       6 9 1
+       7 10 1
+       8 11 1
+       8 12 1))
+
+(deftest create-bloom-test
+  (is (= {:m 1 :n 2 :k 3 :f 0} @(create-bloom 1 2 3)))
+  (is (= {:m 100 :n 10 :k 7 :f 0} @(create-bloom 100 10))))
+
+(deftest bytes->num-test
+  (are [x y] (= x (Long/toString (bytes->num (byte-array (map byte y))) 2))
+       "0" [0]
+       "11111111" [255]
+       "1111111100000000" [255 0]
+       "11110000" [240]
+       "1111" [15]
+       "111111110000000011110000111111110000111100000000" [255 0 240 255 15 0]))
+
+(deftest hash-test
+  (are [x s] (= x (hash s))
+       975987071262755080377722350727279193143145743181  "hello"
+       708652540093010131229728076251103780790279585603  "world"
+       1245845410931227995499360226027473197403882391305 ""))
+
+(deftest indexes-test
+  (are [x ps] (= x (apply indexes ps))
+       [3 1 4] ["a" 10 3]
+       [34 30 68 2 42] ["b" 100 5])
+  (let [m 20
+	k 5]
+    (is (every? #(< % m) (indexes "x" m k)))
+    (is (= k (count (indexes "x" m k))))))
+
+(deftest add-test
+  (let [b (create-bloom 16 4 1)]
+    (add b "a") ;; flips bit 4
+    (is (= "10000" (Integer/toString (:f @b) 2))))
+  (let [b (create-bloom 16 4 3)]
+    (add b "b") ;; flips bits 0, 2 and 14
+    (is (= "100000000000101" (Integer/toString (:f @b) 2)))))
+
+(deftest contains?-test
+  (let [b (create-bloom 64 8 4)]
+    (add b "a")
+    (add b "b")
+    (add b "c")
+    (is (contains? b "a"))
+    (is (contains? b "b"))
+    (is (contains? b "c"))
+    (is (not (contains? b "d")))
+    (is (not (contains? b "e")))
+    (is (not (contains? b "f")))))
