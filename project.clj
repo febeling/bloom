cc2264ce831d175c4cef47394717e7946f4fce4d
Caspar Florian Ebeling <florian.ebeling@nugg.ad>
2010-05-31 10:40:31 +0200

    bloom: increment version to 0.4.1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bloom "0.4.0-SNAPSHOT"
+(defproject bloom "0.4.1-SNAPSHOT"
   :description "Bloom filter implementation in Clojure"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]])
