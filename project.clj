cfe040ba91f3da5d3db92beeb2512fd7c9a9bd21
Roman Scherer <roman.scherer@nugg.ad>
2010-08-03 16:04:50 +0200

    Switched to clojure 1.2 and lein-javac 1.2.1.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject bloom "0.4.2-SNAPSHOT"
   :description "Bloom filter implementation in Clojure"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[lein-javac "0.0.3-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
+                 [org.clojure/clojure-contrib "1.2.0-RC1"]]
+  :dev-dependencies [[lein-javac "1.2.1-SNAPSHOT"]]
   :java-source-path "java"
   :compile-path "classes")
