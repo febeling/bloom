06e7eea78436c649ae6fe79b1bd472602a7d69ae
Roman Scherer <roman.scherer@nugg.ad>
2010-08-03 16:07:09 +0200

    Added swank-clojure 1.2.1 to dev dependencies.

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Bloom filter implementation in Clojure"
   :dependencies [[org.clojure/clojure "1.2.0-RC1"]
                  [org.clojure/clojure-contrib "1.2.0-RC1"]]
-  :dev-dependencies [[lein-javac "1.2.1-SNAPSHOT"]]
+  :dev-dependencies [[lein-javac "1.2.1-SNAPSHOT"]
+                     [swank-clojure "1.2.1"]]
   :java-source-path "java"
   :compile-path "classes")
