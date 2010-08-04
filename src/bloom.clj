79a593424be5e1d42ca87db39f454f15876ee34b
Roman Scherer <roman.scherer@nugg.ad>
2010-08-04 14:42:14 +0200

    Using byteValue instead of the byte fn for coercing.

--- src/bloom.clj
+++ src/bloom.clj
@@ -37,14 +37,6 @@ elements N and number of hash functions K. K can be calculated."
 (defn abit-bit-pos [byte-array i]
   (rem i 8))
 
-;; CLOJURE 1.1
-
-;; (defn byte
-;;   "Coerce to byte"
-;;   {:tag Byte
-;;    :inline (fn [x] `(. clojure.lang.RT (byteCast ~x)))}
-;;   [#^Number x] (. x (byteValue)))
-
 (defn abit-set [byte-array i]
   (let [apos (abit-array-pos byte-array i)
 	bpos (abit-bit-pos byte-array i)]
@@ -53,31 +45,7 @@ elements N and number of hash functions K. K can be calculated."
 	       (-> #^bytes byte-array
                    (aget #^Integer apos)
                    (bit-set bpos)
-                   byte))
-    byte-array))
-
-;; CLOJURE 1.2
-
-;; (defn byte
-;;   "Coerce to byte"
-;;   {:tag Byte
-;;    :inline (fn [x] `(. clojure.lang.RT (byteCast ~x)))
-;;    :added "1.0"}
-;;   [^Number x] (clojure.lang.RT/byteCast x))
-
-(defn byte-1
-  "Coerce to byte"
-  [#^Number x] (. x (byteValue)))
-
-(defn abit-set [byte-array i]
-  (let [apos (abit-array-pos byte-array i)
-	bpos (abit-bit-pos byte-array i)]
-    (aset-byte byte-array
-	       apos
-	       (-> #^bytes byte-array
-                   (aget #^Integer apos)
-                   (bit-set bpos)
-                   (byte-1)))
+                   (.byteValue)))
     byte-array))
 
 (defn abit-test [byte-array i]
