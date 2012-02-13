6c8c22fe16d30698f5229cf851e7314a285f5224
Roman Scherer <roman.scherer@nugg.ad>
2012-02-13 13:56:11 +0100

    Release 0.5.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bloom "0.5.0-SNAPSHOT"
+(defproject bloom "0.5.0"
   :description "Bloom filter implementation in Clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
 		 [com.github.febeling/general-hash-functions "1.1.0"]]
